a19019ac96ad65fef0d4888d3181e58482406de7
陈成 <chencheng@jinlinbao.com>
2019-07-25 17:38:35 +0800

    first commit

---
+++ .vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  base: '/jsm/'
+  title: '急速卖',
+  themeConfig: {
+    search: false,
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '使用', link: '/download/' },
+    ],
+    sidebarDepth: 2,
+  }
+}
---
+++ download/README.md
@@ -0,0 +1,8 @@
+---
+title: 小程序!
+---
+
+<div align=center style="marin-top:200px">
+    <img src="/weixin.jpg" />
+</div>
+
